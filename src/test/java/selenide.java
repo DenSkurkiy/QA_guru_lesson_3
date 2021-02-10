d730ff44a2a85f556c2cdd15b549d165303c5387
denis <den.cross87@gmsil.com>
2021-02-10 13:15:05 +0200

    DragAndDrop

--- src/test/java/selenide.java
+++ src/test/java/selenide.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class selenide {
@@ -18,12 +19,28 @@ public class selenide {
         $(By.xpath("//*[@id=\"js-repo-pjax-container\"]/div[1]/nav/ul/li[5]/a")).click();
         //  Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $(By.id("wiki-pages-filter")).setValue("SoftAssertions").pressEnter();
+        //$(By.className("Box-row"), 16).click();
         $(By.xpath("//*[@id=\"wiki-pages-box\"]/div/div[2]/ul/li[15]/strong/a")).click();
         // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-        $(By.id("wiki-content")).shouldHave(Condition.text("JUnit5 extension"));
+        $(By.id("wiki-content")).shouldHave(text("JUnit5 extension"));
 
 
-        Selenide.sleep(2000);
+        Selenide.sleep(9000);
+
+    }
+
+    @Test
+    void DragAndDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().moveToElement($("#column-a"))
+                .clickAndHold()
+                .moveToElement($("#column-b"),300,200).release().build().perform();
+
+        $("#column-b").shouldHave(text("A"));
+        $("#column-a").shouldHave(text("B"));
+
+        Selenide.sleep(5000);
+
 
     }
 }
